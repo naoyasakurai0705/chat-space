835fb1d67bfd61bb694b3537f30df8b46a3e5936
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-07-11 18:11:52 +0900

    途中

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,23 @@
 $(function(){
-  $('.chat-main__footer-form').on('submit', function(e){
+  function buildHTML(message){
+    var html = `<div class="chat-main__body--messages-list">
+                  <div class="chat-main__body--message">
+                    <div class="chat-main__body--message-name">
+                      ${ message.user.name }
+                    </div>
+                    <div class="chat-main__body--message-time">
+                      ${ message.created_at.strftime("%Y/%m/%d %H:%M") }
+                    </div>
+                    <div class="chat-main__body--message-body">
+                      ${ message.body if message.body.present? }
+                      ${ image_tag message.image.url,class: 'message__image'if message.image.present? }
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -11,4 +29,13 @@ $(function(){
       processData: false,
       contentType: false
   })
-})
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html)
+      $('.message').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.user_id  @message.user_id
+json.group_id  @message.group_id
+json.body  @message.body
+json.image  @message.image
+json.user_name  @message.user.name
+json.created_at  @message.created_at
