a7b500a42bd9de38a4497795dbaeff06a32ad993
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-07-16 20:54:28 +0900

    リファクタリング
    空行、表示方法の統一

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,23 +1,17 @@
 $(function() {
-
-var search_list = $("#user-search-result");
-
 function appendUser(user){
   var html =
     `<div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${user.name}</p>
       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
     </div>`
-  search_list.append(html);
+  $("#user-search-result").append(html);
   }
-
-
 function appendNoUser(user){
   var html =
     `<div class='chat-group-user clearfix'>${user}</div>`
-    search_list.append(html);
+    $("#user-search-result").append(html);
   }
-
 function appendMember(id, name){
   var html =
     `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
@@ -28,11 +22,8 @@ function appendMember(id, name){
   $("#chat-group-users").append(html);
   }
 
-
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
-
         $.ajax({
       type: 'GET',
       url: '/users',
@@ -41,7 +32,8 @@ function appendMember(id, name){
     })
    .done(function(users) {
      $("#user-search-result").empty();
-     if (users.length !== 0) {
+    if members.id = users.id
+    if (users.length !== 0) {
        users.forEach(function(user){
          appendUser(user);
        });
